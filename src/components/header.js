0d3af70dd1946c6397024be3b59bcaad2baaa6fd
Roxana Chiorean <roxana.gherle@gmail.com>
2019-06-24 22:50:37 +0300

    Added news filtering

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import '../css/styles.css'
 
 
-class Header extends Component {
-
-    inputChangeHandler(event){
-        console.log(event.target.value)
-    }
-
-
-    render() {
-        return (
-            <header>
-                <div className='logo'>Logo</div>
-                <input type="text" onChange={this.inputChangeHandler}/>
-            </header>
-        )
-    }
+const Header = (props) => {
+    
+    return (
+        <header>
+            <div className='logo'>Logo</div>
+            <input type="text" onChange={props.keywords}/>
+        </header>
+    )
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -10,14 +10,28 @@ import NewsList from './components/news_list'
 class App extends Component {
 
     state = {
-        news:JSON
+        news:JSON,
+        filtered: []
+    }
+
+    getKeyword = (event) => {
+        let keyword = event.target.value;
+        let filtered = this.state.news.filter((item)=>{
+            return item.title.indexOf(keyword) > -1
+        });
+        this.setState({
+            filtered
+        })
+
     }
 
     render() {
+        let newsFiltered = this.state.filtered;
+        let newsAll = this.state.news;
         return (
             <div>
-                <Header/>
-                <NewsList news={this.state.news}/>
+                <Header keywords={this.getKeyword}/>
+                <NewsList news={newsFiltered.length === 0 ? newsAll : newsFiltered}/>
             </div>
         )
     }
